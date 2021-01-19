ff5a2da2378b3dd8dd9d06d6923b000f1e9ea033
Sean Murrin <seanmurrin@gmail.com>
2021-01-19 11:54:32 +0000

    Chore: update scripts.php

--- library/scripts.php
+++ library/scripts.php
@@ -1,57 +1,73 @@
 <?php
-// Add an async tag to our main-js
-// http://matthewhorne.me/defer-async-wordpress-scripts
+
+/**
+ * Add an async tag to our main-js
+ *
+ * https://matthewhorne.me/defer-async-wordpress-scripts
+ *
+ * @param string $tag
+ * @param string $handle
+ * @return string
+ */
 function add_async_attribute($tag, $handle)
 {
-    if ('main-js' !== $handle)
+    if ('main-js' !== $handle) {
         return $tag;
+    }
+
     return str_replace(' src', ' async src', $tag);
+
+    // COOKIEBOT EXAMPLE - https://support.cookiebot.com/hc/en-us/articles/360009063660-Disable-automatic-cookie-blocking-for-a-specific-script
+    // return str_replace(' src', ' data-cookieconsent="ignore" async src', $tag);
 }
 
-// load our scripts
-function scripts_and_styles()
+add_filter('script_loader_tag', 'add_async_attribute', 10, 2);
+
+/**
+ * Manage Scripts
+ *
+ * @return void
+ */
+function manageScripts()
 {
     if (!is_admin()) {
         // get the filename of main since it can change itself
         // glob is a bit of a weird one i know but there'll only be one main js in there
         $fileLocation = glob('app/themes/bedrock-theme/static/js/main-*.js')[0];
         $jsFile = substr($fileLocation, strrpos($fileLocation, '/') + 1);
-        // register modernizr
-        // wp_register_script('modernizr', get_stylesheet_directory_uri() . '/static/js/modernizr.custom.js', array(), '1.0', false);
+
         // register main scripts in footer
-        wp_register_script('main-js', get_stylesheet_directory_uri() . '/static/js/' . $jsFile, false, NULL, true);
+        wp_register_script('main-js', get_stylesheet_directory_uri() . '/static/js/' . $jsFile, false, null, true);
+
         // queue them up
-        // wp_enqueue_script('modernizr');
         wp_enqueue_script('main-js');
+
+        // nonce
+        wp_localize_script('main-js', 'wpApiSettings', [
+            'root' => esc_url_raw(rest_url()),
+            'nonce' => wp_create_nonce('wp_rest')
+        ]);
+
+        // remove WP embeds
+        wp_deregister_script('wp-embed');
+
+        // dequeue jQuery...actually no we can't because of some plugins!
+        // if (!is_admin_bar_showing()) {
+        //     wp_deregister_script('jquery');
+        //     wp_register_script('jquery', false);
+        //     // wp_dequeue_script('jquery');
+        //     // wp_dequeue_script('jquery-form');
+        // }
+
+        // Remove Gutenberg Block Library CSS from loading on the frontend
+        wp_dequeue_style('wp-block-library');
+        wp_dequeue_style('wp-block-library-theme');
+        wp_dequeue_style('wc-block-style'); // Remove WooCommerce block CSS
+
         // remove annoying WP emoji galore
         remove_action('wp_head', 'print_emoji_detection_script', 7);
         remove_action('wp_print_styles', 'print_emoji_styles');
-        wp_localize_script('main-js', 'wpApiSettings', array(
-            'root' => esc_url_raw(rest_url()),
-            'nonce' => wp_create_nonce('wp_rest')
-        ));
-        // remove WP embeds
-        wp_deregister_script('wp-embed');
-        // dequeue jQuery
-        wp_dequeue_script('jquery');
-        wp_dequeue_script('jquery-form');
-        // dequeue Contact Form 7
-        // wp_dequeue_style('contact-form-7-css');
     }
 }
 
-add_filter('wpcf7_load_js', '__return_false');
-// add a new image size
-//add_image_size('blog_thumbnail', 420, 300, true);
-add_filter('script_loader_tag', 'add_async_attribute', 10, 2);
-add_action('wp_enqueue_scripts', 'scripts_and_styles', 999);
-add_filter('shortcode_atts_wpcf7', 'custom_shortcode_atts_wpcf7_filter', 10, 3);
-
-function custom_shortcode_atts_wpcf7_filter($out, $pairs, $atts)
-{
-    $my_attr = 'current_page';
-    if (isset($atts[$my_attr])) {
-        $out[$my_attr] = $atts[$my_attr];
-    }
-    return $out;
-}
+add_action('wp_enqueue_scripts', 'manageScripts', 999);
