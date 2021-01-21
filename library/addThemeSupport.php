e5fd07cc3a0bedcf18809074562e70effbbd34f0
Suzie Douglas <suziedouglas95@gmail.com>
2021-01-21 17:37:13 +0000

    Feat: Took ThemeSupport out of a class

--- library/addThemeSupport.php
+++ library/addThemeSupport.php
@@ -4,46 +4,59 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
+/**
+ * Add default posts and comments RSS feed links to head.
+ */
+add_theme_support('automatic-feed-links');
 
-function addThemeSupport()
-{
-    // Add default posts and comments RSS feed links to head
-    add_theme_support('automatic-feed-links');
+/**
+ * Let WordPress manage the document title.
+ * By adding theme support, we declare that this theme does not use a
+ * hard-coded <title> tag in the document head, and expect WordPress to
+ * provide it for us.
+ */
+add_theme_support('title-tag');
 
-    // Let WordPress manage the document title
-    add_theme_support('title-tag');
+/**
+ * Enable support for Post Thumbnails on posts and pages.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+ */
+add_theme_support('post-thumbnails');
 
-    // Enable support for Post Thumbnails
-    add_theme_support('post-thumbnails');
+/**
+ * Switch default core markup for search form, comment form, and comments
+ * to output valid HTML5.
+ */
+add_theme_support(
+    'html5',
+    array(
+        'comment-form',
+        'comment-list',
+        'gallery',
+        'caption',
+    )
+);
 
-    // Enable support for Menus
-    add_theme_support('menus');
+/**
+ * Enable support for Post Formats.
+ *
+ * See: https://codex.wordpress.org/Post_Formats
+ */
+add_theme_support(
+    'post-formats',
+    array(
+        'aside',
+        'image',
+        'video',
+        'quote',
+        'link',
+        'gallery',
+        'audio',
+    )
+);
 
-    // Adding excerpt for pages (other post types are done when registered)
-    add_post_type_support('page', 'excerpt');
-
-    // Switch default core markup for search form, comment form, and comments to output valid HTML5
-    add_theme_support(
-        'html5',
-        array(
-            'comment-form',
-            'comment-list',
-            'gallery',
-            'caption',
-        )
-    );
-
-    // Enable support for Post Formats
-    add_theme_support(
-        'post-formats',
-        array(
-            'aside',
-            'image',
-            'video',
-            'quote',
-            'link',
-            'gallery',
-            'audio',
-        )
-    );
-}
+/**
+ * Add menu support
+ */
+add_theme_support('menus');
