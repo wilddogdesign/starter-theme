63969c7e69ceebcec296b4780bf598f86d6f87fb
Suzie Douglas <suziedouglas95@gmail.com>
2021-05-07 10:21:06 +0100

    Feat: Update convertACFphp2json

--- admin/convertACFphp2json.php
+++ admin/convertACFphp2json.php
@@ -1,32 +1,61 @@
 <?php
 
-$status = [];
+/**
+ * Detect plugin. For use on Front End only.
+ */
+include_once(ABSPATH . 'wp-admin/includes/plugin.php');
 
-$theme_dir = get_stylesheet_directory();
-$json_output_dir = $theme_dir . '/acf-json/';
+$themeDir = get_stylesheet_directory();
+$jsonOutputDir = $themeDir . '/acf-json/';
 
-if (!file_exists($json_output_dir)) {
+if (!file_exists($jsonOutputDir)) {
     die('Create a acf-json/ directory in your current theme.');
 }
 
-$fieldgroups = glob($theme_dir . '/acf-dynamic-fieldgroups/*');
-foreach ($fieldgroups as $fg) {
+$fieldgroups = glob($themeDir . '/acf-dynamic-fieldgroups/*');
 
-    if (file_exists($fg)) {
-        // Include $fg into current context
-        // this will create a $group var
-        include($fg);
-        $json_output_dir = $theme_dir . '/acf-json/';
-        if (file_put_contents($json_output_dir . 'dynamic__' . $group['key'] . '.json', json_encode($group))) {
-            $status[] = 'Created json file ' . $group['key'] . ' for ' . $group['title'] . '.';
+if ($fieldgroups) {
+if (class_exists('SitePress')) {
+    //WPML is activated
+    $languages = apply_filters('wpml_active_languages', NULL, 'orderby=id&order=desc');
+
+    if ($languages) {
+        foreach ($languages as $lang => $language) {
+            $jsonOutputDir = $themeDir . '/acf-json/' . $lang . '/';
+
+            if (!file_exists($jsonOutputDir)) {
+                die('Create a language-specific acf-json/ subfolder directory in your current theme.');
+            }
+
+            foreach ($fieldgroups as $fg) {
+                if (file_exists($fg)) {
+                    // Include $fg into current context
+                    // this will create a $group var
+                    include $fg;
+
+                    $jsonFile = fopen($jsonOutputDir . 'dynamic__' . $group['key'] . '.json', 'w');
+                    fwrite($jsonFile, json_encode($group));
+                    fclose($jsonFile);
+                }
+            }
+        }
+    }
+} else {
+    $jsonOutputDir = $themeDir . '/acf-json/';
+
+    if (!file_exists($jsonOutputDir)) {
+        die('Create a lacf-json/ subfolder directory in your current theme.');
+    }
+
+    foreach ($fieldgroups as $fg) {
+        if (file_exists($fg)) {
+            // Include $fg into current context
+            // this will create a $group var
+            include $fg;
+
+            $jsonFile = fopen($jsonOutputDir . 'dynamic__' . $group['key'] . '.json', 'w');
+            fwrite($jsonFile, json_encode($group));
+            fclose($jsonFile);
         }
     }
 }
-
-// if (count($status)) {
-//     echo implode("<br/>", $status);
-// } else {
-//     echo 'No ACFJson files created.';
-// }
-
-// die();
